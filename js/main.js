fc307bd1d56274196d6197d02f60cf4a6d8debd4
Isaac Guzmán <cguzman@be-it.com.mx>
2025-06-03 00:14:09 -0600

    feat: Integración de nueva estructura HTML, complementos nuevos en secciones.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
-$(function(){
-    $('a.nav-link').on('click', function(event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 1000, function(){
-                window.location.hash = hash;
-            });
-        }
-    });
-    
+$(document).ready(function () {
+  function activateLink(hash) {
+    $('.menu-lateral .nav-link').removeClass('active');
+    $('.menu-lateral .nav-link[href="' + hash + '"]').addClass('active');
+  }
+
+  // Al hacer clic
+  $('.menu-lateral .nav-link').on('click', function (e) {
+    e.preventDefault();
+    const target = $(this).attr('href');
+    const offsetTop = $(target).position().top + $('#exp-container').scrollTop();
+
+    $('#exp-container').animate({
+      scrollTop: offsetTop
+    }, 500);
+
+    activateLink(target);
+  });
+
+  // Al cargar la página con hash
+  const initialHash = window.location.hash;
+  if (initialHash) {
+    activateLink(initialHash);
+  }
 });
