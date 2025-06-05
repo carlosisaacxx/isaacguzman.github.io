b2f141a6c8a3fde2934d323fb7ffcfc95c5ee3f9
Isaac Guzmán <cguzman@be-it.com.mx>
2025-06-04 23:55:59 -0600

    feat: Actualización de la experiencia en 2025 y 2024, así como mejoras visuales y estructura en la sección de experiencia

---
+++ js/date.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  const mounth = [
+    "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+    "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+  ];
+
+  const date = new Date();
+  const nameMounth = mounth[date.getMonth()];
+  const year = date.getFullYear();
+
+  const dateFormatter = `${nameMounth} ${year}`;
+  $("#present-day").text(dateFormatter);
+});
--- js/main.js
+++ js/main.js
@@ -1,25 +1,2 @@
 $(document).ready(function () {
-  function activateLink(hash) {
-    $('.menu-lateral .nav-link').removeClass('active');
-    $('.menu-lateral .nav-link[href="' + hash + '"]').addClass('active');
-  }
-
-  // Al hacer clic
-  $('.menu-lateral .nav-link').on('click', function (e) {
-    e.preventDefault();
-    const target = $(this).attr('href');
-    const offsetTop = $(target).position().top + $('#exp-container').scrollTop();
-
-    $('#exp-container').animate({
-      scrollTop: offsetTop
-    }, 500);
-
-    activateLink(target);
-  });
-
-  // Al cargar la página con hash
-  const initialHash = window.location.hash;
-  if (initialHash) {
-    activateLink(initialHash);
-  }
 });
